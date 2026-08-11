b722d00e9bbd33a2087d75b477409906409da825
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-08-11 11:18:30 +0530

    Create calculation_main.c

---
+++ calculation_main.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,sub,mul,div,add;
+
+printf("Enter the first number :");
+scanf("%d",&a);
+printf("Enter the second number :");
+scanf("%d",&b);
+sub =a-b;
+mul =a*b;
+div =a/b;
+add =a+b;
+printf("calculation of two numbers ;%d\n",sub);
+printf("calculation of two numbers :%d\n",mul);
+printf("calculation of two numbers :%d\n",div);
+printf("calculation of two numbers :%d\n",add);
+return 0;
+}
