7e1b08008f7fe36f0d808b322ae936e8d4667c8b
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-08-11 18:38:57 +0530

    Create predifinednumber.c

---
+++ predifinednumber.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,b,add,sub,mul,div;    
+ a =60;
+ b =10;
+add =a+b;
+sub =a-b;
+mul =a*b;
+div =a/b;
+printf("addition of two numbers:%d\n", add);
+printf("substraction of two numbers:%d\n", sub);
+printf("multipication of two numbers:%d\n", mul);
+printf("divission of two numbers:%d \n", div);
+return 0;
+}
