b6fdcaeebd69405da094cd1253e1da09079631b5
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-08-16 20:36:30 +0530

    Create calculate_square root_.c

---
+++ calculate_square root_.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float num,result;
+printf("enter the num:");
+scanf("%f",&num);
+result =sqrt(num);
+printf("calculate the result:%.2f",result);
+return 0;
+}
