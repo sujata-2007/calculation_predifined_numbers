503760113360a5ced8800ccf3607f8a0a436fa25
Sujata Biswas <sujatabiswas20007@gmail.com>
2026-08-24 07:12:15 +0530

    calculate to cast a vote_.c

---
+++ calculate to cast a vote_.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    int age;
+    char c[20];
+    printf("you are an indian citizen (yes/no):");
+    scanf("%s",&c)
+    
+    printf("enter your age:");
+    scanf("%d",&age);
+    if(age>=18 && (strcmp(c,"yes")==0))
+    {
+        printf("you are eligible to vote");
+    }
+    else
+    {
+        printf("tou are not eligible to vote");
+    }
+    return 0;
+}
